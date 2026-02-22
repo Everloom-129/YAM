fcc651554b15442b331676f6d1bc1f2359e5b648
williamtsai726 <115367872+williamtsai726@users.noreply.github.com>
2026-02-22 12:18:31 +0800

    Update add_tag.py to take terminal parameter

--- add_tag.py
+++ add_tag.py
@@ -1,10 +1,16 @@
 from huggingface_hub import HfApi
+from dataclasses import dataclass
+
+@dataclass
+class Args:
+    repo_id: str
+    """Path to the huggingface repo."""
 
 hub_api = HfApi()
 
-hub_api.create_tag("williamtsai726/wipe_the_dish_0125", tag="v3.0", repo_type="dataset")
+hub_api.create_tag(args.repo_id, tag="v3.0", repo_type="dataset")
 
-refs = hub_api.list_repo_refs("williamtsai726/wipe_the_dish_0125", repo_type="dataset")
+refs = hub_api.list_repo_refs(args.repo_id, repo_type="dataset")
 
 # tags are under refs.tags
 print([tag.name for tag in refs.tags])
