6bff3fbe87fa20ee7ba6c9a36cb46fe9c2861481
williamtsai726 <williamtsai726@gmail.com>
2026-02-26 21:12:39 -0800

    changed linear gripper limit to maximum.

--- i2rt/i2rt/robots/utils.py
+++ i2rt/i2rt/robots/utils.py
@@ -50,7 +50,7 @@ class GripperType(enum.Enum):
         if self == GripperType.CRANK_4310:
             return 0.0, -2.7
         elif self in [GripperType.LINEAR_3507, GripperType.LINEAR_4310]:
-            return [0.0, -2.4]
+            return [0.0, -5.2]
             # return None
         elif self in [GripperType.YAM_TEACHING_HANDLE, GripperType.NO_GRIPPER]:
             return None
